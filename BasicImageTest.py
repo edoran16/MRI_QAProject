10d66f264f4c080476f25eef02ed786d2fd3b4f1
GU Student <emma.doran@btconnect.com>
2020-04-30 15:15:10 +0100

    Day 4 commit 1

---
+++ BasicImageTest.py
@@ -0,0 +1,8 @@
+from DICOM_test import dicom_read_and_write
+
+directpath = "data_to_get_started/single_slice_dicom/"
+# filename = "image1"
+# path = "{0}{1}".format(directpath, filename)
+# ds, imdata, dims = dicom_read_and_write(path)
+
+
