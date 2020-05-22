4a60fc7bfc915ec2320f66dbb3c2fdd035295008
GU Student <emma.doran@btconnect.com>
2020-05-22 17:40:55 +0100

    magNET slice position measurements on single slice

--- magNET_ImageTest_SlicePos.py
+++ magNET_ImageTest_SlicePos.py
@@ -6,6 +6,7 @@ import numpy as np
 import os
 import sys
 import cv2
+import pandas as pd
 import imutils
 
 from scipy.spatial import distance as dist
@@ -34,6 +35,23 @@ with os.scandir(pathtodicom) as it:
         path = "{0}{1}".format(pathtodicom, file.name)
 
 ds, imdata, df, dims = dicom_read_and_write(path)  # function from DICOM_test.py
+
+
+def slice_pos_meta(dicomfile):
+    # extract metadata for slice postion info calculations
+    # dicomfile = pydicom.dataset.FileDataset
+    elem = dicomfile[0x5200, 0x9230]  # pydicom.dataelem.DataElement, (Per-frame Functional Groups Sequence)
+    seq = elem.value  # pydicom.sequence.Sequence
+    elem3 = seq[0]  # first frame
+    elem4 = elem3.PixelMeasuresSequence  # pydicom.sequence.Sequence
+
+    for xx in elem4:
+        st = xx.SliceThickness
+        slice_space = xx.SpacingBetweenSlices
+        pixels_space = xx.PixelSpacing
+
+    return st, slice_space, pixels_space
+
 
 try:
     xdim, ydim, zdim = dims
@@ -97,10 +115,8 @@ phim = imdata[zz, :, :]*phmask  # phantom image
 bgim = imdata[zz, :, :]*~phmask  # background image
 
 # display image
-plt.figure()
-plt.imshow(phim, cmap='bone')
-plt.axis('off')
-plt.show()
+cv2.imshow('phantom image', ((phim/np.max(phim))*255).astype('uint8'))
+cv2.waitKey(0)
 
 phim_dims = np.shape(phim)
 
@@ -109,29 +125,25 @@ phim_gray = phim_norm*255
 
 edged = cv2.Canny(phim_gray.astype('uint8'), 20, 200)
 bigbg = cv2.dilate(~phmask.astype('uint8'), None, iterations=4)  # dilate background mask
+bigbg[bigbg == 254] = 0
 
 edged = edged*~bigbg
 
-plt.figure()
-plt.subplot(141)
-plt.imshow(bigbg)
-plt.axis('off')
-plt.subplot(142)
-plt.imshow(edged)
-plt.axis('off')
+cv2.imshow('Dilated Background', bigbg)
+cv2.waitKey(0)
+
+cv2.imshow('Canny Filter', edged.astype('float32'))
+cv2.waitKey(0)
 
 edgedd = cv2.dilate(edged, None, iterations=1)
 
-plt.subplot(143)
-plt.imshow(edgedd)
-plt.axis('off')
+cv2.imshow('Canny Dilated', edgedd.astype('float32'))
+cv2.waitKey(0)
 
 edgede = cv2.erode(edgedd, None, iterations=1)
 
-plt.subplot(144)
-plt.imshow(edgede)
-plt.axis('off')
-plt.show()
+cv2.imshow('Canny Eroded', edgede.astype('float32'))
+cv2.waitKey(0)
 
 lines_im = phmask.copy()
 
@@ -149,18 +161,14 @@ for lineno in np.linspace(0, no_lines-1, no_lines, dtype=int):
 
 label_this = edgede*lines_im
 
-plt.figure()
-plt.subplot(121)
-plt.imshow(label_this)
-plt.axis('off')
+cv2.imshow('Rods Detected', label_this.astype('float32'))
+cv2.waitKey(0)
 
 label_img, num = label(label_this, connectivity=phim_gray.ndim, return_num=True)  # labels the mask
 print('Number of regions detected (should be 6!!!) = ', num)
 
-plt.subplot(122)
-plt.imshow(label_img)
-plt.axis('off')
-plt.show()
+cv2.imshow('Rods labelled', label_img.astype('float32'))
+cv2.waitKey(0)
 
 props = regionprops(label_img)  # returns region properties for labelled image
 cent = np.zeros([num, 2])
@@ -176,10 +184,9 @@ for i in cent:
     # draw the center of the circle
     cv2.circle(marker_im, (i[0], i[1]), 1, 0, 1)
 
-plt.figure()
-plt.imshow(marker_im)
-plt.axis('off')
-plt.show()
+marker_im = marker_im*255
+cv2.imshow('marker image', marker_im.astype('uint8'))
+cv2.waitKey(0)
 
 temp1 = []
 temp2 = []
@@ -193,107 +200,105 @@ for i in cent:
     if i[1] > 180:
         temp3.append(i)
 
+# top 2 parallel rods
 src1 = temp1[0]
 src1 = (src1[0], src1[1])
 dst1 = temp1[1]
 dst1 = (dst1[0], dst1[1])
 
+# angled rods
 src2 = temp2[0]
 src2 = (src2[0], src2[1])
 dst2 = temp2[1]
 dst2 = (dst2[0], dst2[1])
 
+# bottom 2 parallel rods
 src3 = temp3[0]
 src3 = (src3[0], src3[1])
 dst3 = temp3[1]
 dst3 = (dst3[0], dst3[1])
 
-cv2.line(marker_im, src1, dst1, 0, 1)
-cv2.line(marker_im, src2, dst2, 0, 1)
-cv2.line(marker_im, src3, dst3, 0, 1)
 
-plt.figure()
-plt.imshow(marker_im)
-plt.axis('off')
-plt.show()
+#TODO: repalce marker_im with RGB/grayscale image of phantom and draw coloured lines on image instead
+hmarker_im = marker_im.copy()  # for horizontal lines
+vmarker_im = marker_im.copy()  # for vertical lines
 
+cv2.line(hmarker_im, src1, dst1, 0, 1)
+# cv2.line(hmarker_im, src2, dst2, 0, 1)  # diagonal line between angled rods
+cv2.line(hmarker_im, (src2[0], int(phim_dims[1]/2)), (dst2[0], int(phim_dims[1]/2)), 0, 1)  # horizontal lines between angled rods
+cv2.line(hmarker_im, src3, dst3, 0, 1)
 
-# # find contours in the edge map
-# cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# cnts = imutils.grab_contours(cnts)
-#
-# # sort the contours from left-to-right and initialize the
-# # 'pixels per metric' calibration variable
-# (cnts, _) = contours.sort_contours(cnts)
-# pixelsPerMetric = None
-#
-#
-# # loop over the contours individually
-# for c in cnts:
-#     # if the contour is not sufficiently large, ignore it
-#     if cv2.contourArea(c) >= 7:
-#         continue
-#     # compute the rotated bounding box of the contour
-#     print(cv2.contourArea(c))
-#     orig = phim_gray.copy()
-#     box = cv2.minAreaRect(c)
-#     box = cv2.boxPoints(box)
-#     box = np.array(box, dtype="int")
-#     # order the points in the contour such that they appear
-#     # in top-left, top-right, bottom-right, and bottom-left
-#     # order, then draw the outline of the rotated bounding
-#     # box
-#     box = perspective.order_points(box)
-#     cv2.drawContours(orig, [box.astype("int")], -1, 0, 2)
-#     # loop over the original points and draw them
-#
-#     for (x, y) in box:
-#         cv2.circle(orig, (int(x), int(y)), 5, 0, -1)
-#
-#     cv2.imshow('orig', orig)
-#     cv2.waitKey(0)
-#
-#     # # unpack the ordered bounding box, then compute the midpoint
-#     # # between the top-left and top-right coordinates, followed by
-#     # # the midpoint between bottom-left and bottom-right coordinates
-#     # (tl, tr, br, bl) = box
-#     # (tltrX, tltrY) = midpoint(tl, tr)
-#     # (blbrX, blbrY) = midpoint(bl, br)
-#     # # compute the midpoint between the top-left and top-right points,
-#     # # followed by the midpoint between the top-righ and bottom-right
-#     # (tlblX, tlblY) = midpoint(tl, bl)
-#     # (trbrX, trbrY) = midpoint(tr, br)
-#     # # draw the midpoints on the image
-#     # cv2.circle(orig, (int(tltrX), int(tltrY)), 5, 0, -1)
-#     # cv2.circle(orig, (int(blbrX), int(blbrY)), 5, 0, -1)
-#     # cv2.circle(orig, (int(tlblX), int(tlblY)), 5, 0, -1)
-#     # cv2.circle(orig, (int(trbrX), int(trbrY)), 5, 0, -1)
-#     # # draw lines between the midpoints
-#     # cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)), 0, 2)
-#     # cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)), 0, 2)
-#     #
-#     # cv2.imshow('orig', orig)
-#     # cv2.waitKey(0)
-#     #
-#     # # compute the Euclidean distance between the midpoints
-#     # dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-#     # dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-#     # # if the pixels per metric has not been initialized, then
-#     # # compute it as the ratio of pixels to supplied metric
-#     # # (in this case, inches)
-#     # if pixelsPerMetric is None:
-#     #     pixelsPerMetric = dB / 120  # known width. Use 120 for now.
-#     #
-#     # # compute the size of the object
-#     # dimA = dA / pixelsPerMetric
-#     # dimB = dB / pixelsPerMetric
-#     # # draw the object sizes on the image
-#     # cv2.putText(orig, "{:.1f}mm".format(dimA), (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, 255, 1)
-#     # cv2.putText(orig, "{:.1f}mm".format(dimB), (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, 255, 1)
-#     # # show the output image
-#     # cv2.imshow("Image", orig)
-#     # cv2.waitKey(0)
-#     #
-#     # #print(dimA)
-#     # #print(dimB)
-#
+cv2.imshow('horiz. marker image', hmarker_im.astype('uint8'))
+cv2.waitKey(0)
+
+cv2.line(vmarker_im, src1, src3, 0, 1)
+cv2.line(vmarker_im, dst1, dst3, 0, 1)
+#TODO: replace centre of FOV with centre of full phantom
+cv2.line(vmarker_im, (int(phim_dims[0]/2), src2[1]), (int(phim_dims[0]/2), dst2[1]), 0, 1)
+
+cv2.imshow('vert. marker image', vmarker_im.astype('uint8'))
+cv2.waitKey(0)
+
+#compute the Euclidean distance between the midpoints
+# horizontal lines
+hdist1 = dist.euclidean(src1, dst1)
+hdist2 = dist.euclidean((src2[0], int(phim_dims[1]/2)), (dst2[0], int(phim_dims[1]/2)))
+hdist3 = dist.euclidean(src3, dst3)
+
+print('Horizontal distance (top) = ', hdist1, 'mm')
+print('Horizontal distance between angled rods = ', hdist2, 'mm')
+print('Horizontal distance (bottom) = ', hdist3, 'mm')
+
+# horizontal lines
+cv2.putText(hmarker_im, "{:.1f}mm".format(hdist1), (int(phim_dims[0]/2), int(dst1[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, 255, 1)
+cv2.putText(hmarker_im, "{:.1f}mm".format(hdist2), (src2[0] + 30, int(phim_dims[1]/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, 0, 1)
+cv2.putText(hmarker_im, "{:.1f}mm".format(hdist3), (int(phim_dims[0]/2), int(dst3[1] + 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, 0, 1)
+
+cv2.imshow("horiz. measurements", hmarker_im.astype('uint8'))
+cv2.waitKey(0)
+
+# vertical lines
+vdist1 = dist.euclidean(src1, src3)
+vdist2 = dist.euclidean(dst1, dst3)
+#TODO: replace centre of FOV with centre of full phantom
+vdist3 = dist.euclidean((int(phim_dims[0]/2), src2[1]), (int(phim_dims[0]/2), dst2[1]))
+
+print('Vertical distance (left) = ', vdist1, 'mm')
+print('Vertical distance (right) = ', vdist2, 'mm')
+print('Vertical distance between angled rods = ', vdist3, 'mm')
+
+cv2.putText(vmarker_im, "{:.1f}mm".format(vdist1), (int(src1[0] + 5), int(src3[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.3, 0, 1)
+cv2.putText(vmarker_im, "{:.1f}mm".format(vdist2), (int(dst1[0] + 15), int(dst3[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.3, 255, 1)
+cv2.putText(vmarker_im, "{:.1f}mm".format(vdist3), (int(phim_dims[0]/2), int(phim_dims[1]/2) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, 0, 1)
+
+cv2.imshow("vert. measurements", vmarker_im.astype('uint8'))
+cv2.waitKey(0)
+
+# CALCULATIONS
+RDA = [120, 120, 120, 120]
+RDM = [hdist1, hdist3, vdist1, vdist2]
+#print(RDM)
+CFall = np.divide(RDA, RDM)
+CF = np.mean(CFall)
+print('Mean Correction Factor = ', CF)
+
+distance_of_angled_rods = zz#vdist3  # not sure is this the right value here according to Excel sheet.....
+slice_position_measured = np.sqrt(((distance_of_angled_rods*CF)**2) - (6.5**2))/2
+print('Measured position = ', slice_position_measured)
+
+first_slice_position = 1  # get this from header info? is this slice number...?
+slice_position = zz+1
+slice_thickness, slice_gap, pixeldims = slice_pos_meta(ds)
+
+print(slice_thickness, slice_gap, pixeldims)
+
+# this changes with respect to previous position (see Excel sheet!) - will make more sense will extend code to 3D loop
+# over slices
+slice_position_calculated_upper = first_slice_position + ((slice_position - 1)*slice_thickness)
+slice_position_calculated_lower = first_slice_position - ((slice_position - 1)*slice_thickness)
+slice_position_calculated = slice_position_calculated_upper - slice_position_calculated_lower  # this is not correct.
+print('Calculated position = ', slice_position_calculated)
+
+slice_position_error = slice_position_calculated - slice_position_measured
+print('Error = ', slice_position_error, 'mm (must be within +- 1 mm)')
+
