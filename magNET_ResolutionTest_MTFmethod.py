2cc22fd1ea95c12ad31550646113ed98ff8334ef
GU Student <emma.doran@btconnect.com>
2020-06-17 17:26:59 +0100

    LOESS curve fitting for ESF

--- magNET_ResolutionTest_MTFmethod.py
+++ magNET_ResolutionTest_MTFmethod.py
@@ -21,7 +21,7 @@ from skimage.morphology import opening
 directpath = "MagNET_acceptance_test_data/scans/"
 imagepath = "MagNET_acceptance_test_data/Resolution_Images/"
 
-show_graphical = True
+show_graphical = False
 
 with os.scandir(directpath) as the_folders:
     for folder in the_folders:
@@ -60,39 +60,43 @@ except ValueError:
 mask = rf.create_2D_mask(img)  # watch out for grayscale mask!! [0, 255]
 bin_mask = (mask/np.max(mask)).astype('uint8')  # binary mask [0, 1]
 
-cv2.imshow('mask', mask)
-cv2.waitKey(0)
+if show_graphical:
+    cv2.imshow('mask', mask)
+    cv2.waitKey(0)
 
 phim = img*bin_mask  # phantom masked image
 bgim = img*(1-bin_mask)  # background image
 
-cv2.imshow('phantom masked', phim)
-cv2.waitKey(0)
-cv2.imshow('background masked', bgim)
-cv2.waitKey(0)
+if show_graphical:
+    cv2.imshow('phantom masked', phim)
+    cv2.waitKey(0)
+    cv2.imshow('background masked', bgim)
+    cv2.waitKey(0)
 
 # otsu threshold
 ots = np.zeros_like(phim, dtype=np.uint8)
 ots[(phim > filters.threshold_otsu(phim)) == True] = 255  # Otsu threshold on weighted combination
 
-cv2.imshow('Otsu Threshold', ots)
-cv2.waitKey(0)
+if show_graphical:
+    cv2.imshow('Otsu Threshold', ots)
+    cv2.waitKey(0)
 
 erode_mask = cv2.erode(bin_mask, None, iterations=1)
 
 label_this = (255-ots)*erode_mask
 
-cv2.imshow('Label This', label_this)
-cv2.waitKey(0)
+if show_graphical:
+    cv2.imshow('Label This', label_this)
+    cv2.waitKey(0)
 
 # label this
 label_img, num = label(label_this, connectivity=ots.ndim, return_num=True)  # labels the mask
 print('Number of regions detected = ', num)
-plt.figure()
-plt.imshow(label_img)
-plt.show()
 
-sys.exit()
+if show_graphical:
+    plt.figure()
+    plt.imshow(label_img)
+    plt.show()
 
 props = regionprops(label_img)  # returns region properties for labelled image
 cent = np.zeros([num, 2])
@@ -116,15 +120,9 @@ print('Label number to segment = ', idx+1)
 central_block_im = np.zeros(np.shape(label_img))
 central_block_im[label_img == 4] = 255
 
-cv2.imshow('Central Block', central_block_im.astype('uint8'))
-cv2.waitKey(0)
-
-# dilate the central block so that the signal profile can extend before and after the detected edge
-# dilated_block = cv2.dilate(central_block_im, None, iterations=5)
-# central_block_im = dilated_block
-# TODO: think of way to extend beyond the perimeter at angle
-# cv2.imshow('Dilated Central Block', central_block_im.astype('uint8'))
-# cv2.waitKey(0)
+if show_graphical:
+    cv2.imshow('Central Block', central_block_im.astype('uint8'))
+    cv2.waitKey(0)
 
 elements = np.where(central_block_im == 255)  # [y, x], [rows, cols]
 
@@ -170,14 +168,6 @@ cv2.line(marker_im, (min_col, min_col_row), (min_row_col, min_row), (0, 0, 255),
 
 cv2.line(marker_im, (half_col_left, half_row_left), (half_col_right, half_row_right), (255, 0, 0), 1)
 cv2.line(marker_im, (half_col_top, half_row_top), (half_col_bottom, half_row_bottom), (255, 0, 0), 1)
-
-# """ Rotate image"""
-# cv2.imshow('img', img.astype('uint8'))
-# cv2.waitKey(0)
-# # rotation angle in degree
-# rotated = ndimage.rotate(img, 350)
-# cv2.imshow('rotated img', rotated.astype('uint8'))
-# cv2.waitKey(0)
 
 if show_graphical:
     cv2.imwrite("{0}marker_image.png".format(imagepath), marker_im.astype('uint8'))
@@ -243,24 +233,37 @@ improfile[np.array(np.round(perp_rows), dtype=int), np.array(np.round(perp_cols)
 """EDGE RESPONSE FUNCTION"""
 # voxel values along specified line. specify row then column (y then x)
 output = profile_line(img, (src[1], src[0]), (dst[1], dst[0]))  # signal profile
-voxels = np.arange(0, len(output)) - int(len(output)/2)   # define 0 mm as edge cross section
-soutput = rf.smooth(output, window_len=11)  # smoothed signal profile
+"""VOXEL X AXIS VARIABLE"""
+# voxels = np.arange(0, len(output)) - int(len(output)/2)   # define 0 mm as edge cross section
+voxels = np.arange(0, len(output))  # voxel number
 
-# plt.figure()
-# plt.plot(voxels, output, voxels, soutput)
-# plt.legend(['Signal Profile', 'Smoothed Signal Profile'])
-# plt.show()
+smooth_len = int(0.5*len(output))  # has to be an odd value
+smooth_check = smooth_len % 2
+if smooth_check == 0:  # if smooth_len is even
+    smooth_len = smooth_len + 1
+soutput = rf.smooth(output, window_len=smooth_len)  # smoothed signal profile
+
+plt.figure()
+plt.plot(voxels, output, voxels, soutput)
+plt.legend(['Signal Profile', 'Smoothed Signal Profile'])
+plt.title('Smoothing Step Response')
+plt.show()
 
 # FOLLOWING THE ANALYSIS IN THIS PAPER vv
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6236841/pdf/ACM2-19-244.pdf
 f = interpolate.interp1d(voxels, soutput)  # spline interpolation
-voxnew = np.linspace(0, len(soutput)-1, 1000) - int(len(soutput)/2)  # define new voxel/distance vector
+interp_length = 10000
+"""VOXEL X AXIS VARIABLE"""
+# voxnew = np.linspace(0, len(soutput)-1, interp_length) - int(len(soutput)/2)  # define new voxel/distance vector
+voxnew = np.linspace(0, len(soutput)-1, interp_length)  # define new voxel/distance vector
+
 outnew = f(voxnew)   # use interpolation function returned by `interp1d`
 
-# plt.figure()
-# plt.plot(voxels, soutput, 'o', voxnew, outnew, '.')
-# plt.legend(['Smoothed Signal Profile', 'Spline Interpolation'])
-# plt.show()
+plt.figure()
+plt.plot(voxels, soutput, 'o', voxnew, outnew, '.')
+plt.legend(['Smoothed Signal Profile', 'Spline Interpolation'])
+plt.title('Increasing Number of Sample Points')
+plt.show()
 
 outnorm = outnew/np.max(outnew)
 
@@ -271,29 +274,73 @@ ys = outnorm
 xs = voxnew
 
 
-def fit_func(x, a, b):
-    a_in_rad = np.deg2rad(a)  # a in radians
-    tan_a_in_rad = np.tan(a_in_rad)  # tan(a)
-    arctan_xa_product = b*np.arctan(-x * tan_a_in_rad)  # arctan(x*tan(a))
-    arctan_xa_product = arctan_xa_product + np.abs(np.min(arctan_xa_product))  # zero function
-    arctan_xa_product = arctan_xa_product/np.max(arctan_xa_product)  # normalise function
-    return arctan_xa_product
+def fit_func1(x, a, b):
+    f = (a*x) + b  # first order polynomial
+    return f
 
 
-a0 = 45
-b0 = 0.5
-param, param_cov = curve_fit(fit_func, xs, ys, p0=(a0, b0), bounds=([30, 0.4], [60, 0.6]))
-print('Using curve we retrieve a value of a = ', param[0], 'b = ', param[1])
+def fit_func2(x, a, b, c):
+    f = (a*(x**2)) + (b*x) + c  # 2nd order polynomial
+    return f
 
-y = fit_func(xs, param[0], param[1])
-plt.figure(1)
+
+def fit_func3(x, a, b, c, d):
+    f = (a * (x ** 3)) + (b * (x ** 2)) + (c * x) + d  # third order polynomial
+    return f
+
+
+def fit_func4(x, a, b, c, d, e):
+    f = (a * (x ** 4)) + (b * (x ** 3)) + (c * (x**2)) + (d * x) + e  # fourth order polynomial
+    return f
+
+
+param1, param_cov1 = curve_fit(fit_func1, xs, ys)#, p0=(a0, b0, c0), bounds=([30, 0.4, 0], [60, 0.6, 100]))
+print('Using curve we retrieve a value of a = ', param1[0], 'b = ', param1[1])
+y1 = fit_func1(xs, param1[0], param1[1])
+
+param2, param_cov2 = curve_fit(fit_func2, xs, ys)#, p0=(a0, b0, c0), bounds=([30, 0.4, 0], [60, 0.6, 100]))
+print('Using curve we retrieve a value of a = ', param2[0], 'b = ', param2[1], 'c =', param2[2])
+y2 = fit_func2(xs, param2[0], param2[1], param2[2])
+
+param3, param_cov3 = curve_fit(fit_func3, xs, ys)#, p0=(a0, b0, c0), bounds=([30, 0.4, 0], [60, 0.6, 100]))
+print('Using curve we retrieve a value of a = ', param3[0], 'b = ', param3[1], 'c =', param3[2], 'd = ', param3[3])
+y3 = fit_func3(xs, param3[0], param3[1], param3[2], param3[3])
+
+param4, param_cov4 = curve_fit(fit_func4, xs, ys)#, p0=(a0, b0, c0), bounds=([30, 0.4, 0], [60, 0.6, 100]))
+print('Using curve we retrieve a value of a = ', param4[0], 'b = ', param4[1], 'c =', param4[2], 'd = ', param4[3], 'e =', param4[4])
+y4 = fit_func4(xs, param4[0], param4[1], param4[2], param4[3], param4[4])
+
+# y = fit_func(xs, a0, b0, c0, d0)
+plt.figure()
 plt.plot(xs, ys)
-plt.plot(xs, y)
-plt.legend(['ERF Data', 'arctan Fit to ERF'])
+plt.plot(xs, y1)
+plt.plot(xs, y2)
+plt.plot(xs, y3)
+plt.plot(xs, y4)
+plt.legend(['ERF Data', 'Linear Fit', '2nd Order Fit', '3rd Order Fit', '4th Order Fit'])
+plt.title('Increasing Order of Polynomial Fits to Step Response')
+plt.show()
+
+
+"""LOESS CURVE FITTING re: KOHM ET AL. METHOD """
+# https://xavierbourretsicotte.github.io/loess.html
+# https://www.statsmodels.org/devel/generated/statsmodels.nonparametric.smoothers_lowess.lowess.html
+
+import statsmodels.api as sm
+
+lowess = sm.nonparametric.lowess
+z = lowess(ys, xs, return_sorted=False, frac=0.01)
+"""NB: fitting to smoothed, and interpolated introducing additional errors/bias"""
+
+plt.figure()
+plt.plot(xs, ys, 'b', LineWidth=4)
+plt.plot(xs, z, 'r')
+plt.legend(['Line Profile', 'LOESS'])
+plt.title('LOESS Fit to Data')
 plt.show()
 
 # ERF = ys  # smoothed and interpolated data
-ERF = y  # arctan data
+ERF = z  # LOESS data
 
 # ERF
 plt.figure(2)
@@ -302,27 +349,10 @@ plt.plot(voxnew, ERF)
 plt.xlabel('Position (mm)')
 plt.title('Edge Response Function')
 
-"""DIFFERENTIATE FOR LINE SPREAD FUNCTION"""
-# TODO: differentiate properly...
-# from sympy import *
-# x, y, z = symbols('x y z')
-# #f = z*(1/(tan(-x * y)))  # arctan(x*tan(a))
-# f = z*(1/tan(-x*y))
-# df = diff(f, x)
-#
-# print(df)
-#
-# xx = voxnew
-# yy = np.tan(np.deg2rad(param[0]))
-# zz = param[1]
-#
-# diff_lsf = yy*zz*(np.tan(xx*yy)**2 + 1)/np.tan(xx*yy)**2
-#
-# plt.figure(3)
-# plt.plot(voxnew, diff_lsf)
-
+"""NUMERICAL DERIVATIVE OF ERF = LINE SPREAD FUNCTION"""
 diff_output = []
-
+""" the more points that are interpolated, the denominator becomes a smaller value, 
+and this difference method for derivation tends towards the true derivative: CJV """
 for dd in np.linspace(0, len(ERF)-2, len(ERF)-1):
     diff = np.abs(ERF[int(dd+1)] - ERF[int(dd)])
     diff_output.append(diff)
@@ -330,7 +360,7 @@ for dd in np.linspace(0, len(ERF)-2, len(ERF)-1):
 diff_zeroed = diff_output - np.min(diff_output)  # zero the LSF
 diff_norm = diff_zeroed/np.max(diff_zeroed)  # normalise LSF
 LSF = diff_norm
-voxs = np.linspace(np.min(voxnew), np.max(voxnew), len(diff_norm)) #- int(np.max(voxnew)/2)
+voxs = np.linspace(np.min(voxnew), np.max(voxnew), len(diff_norm))
 
 # LSF
 plt.figure(2)
@@ -339,24 +369,35 @@ plt.plot(voxs, LSF)
 plt.xlabel('Position (mm)')
 plt.title('Line Spread Function')
 
-"""FOURIER TRANSFORM FOR MTF"""
+"""FOURIER TRANSFORM OF LSF = MTF"""
+MTF = np.fft.fftshift(np.fft.fft(LSF))  # shifted fourier transform of LSF
+# MTF = np.fft.fft(LSF)  # fourier transform of LSF
+MTF = np.abs(MTF)  # absolute value norm of the FT
+MTF = MTF/np.max(MTF)  # normalised MTF
 
-MTF = np.fft.fftshift(fft(diff_norm))
-# MTF = fft(LSF)  # fourier transform of LSF
-MTF_abs = np.abs(MTF)  # absolute value norm of the FT
-MTF_norm = MTF_abs/np.max(MTF_abs)  # normalised MTF
+# sampling_interval = len(output)/interp_length
+FOV = 250  # FOV specified in MagNET protocol
+matrix_size = 512  # for this particular data set. Can also be 256. TODO: Check DICOM header data
+sampling_interval = FOV/matrix_size
 
-spatial_res = 1/voxs
+no_of_pixels = voxels + 1
+spatial_res = 1/(no_of_pixels * sampling_interval)
+spatial_res = spatial_res[::-1]
+print(spatial_res.shape)
+
+spatial_res_vec = np.linspace(np.min(spatial_res), np.max(spatial_res), len(MTF))
+print(spatial_res)
+print(spatial_res_vec)
 
 # MTF
 plt.figure(2)
 plt.subplot(133)
-#plt.plot(spatial_res, MTF_norm)
-plt.plot(spatial_res, MTF_norm)
+plt.plot(spatial_res_vec, MTF)
 plt.xlabel('Spatial Frequency (cycles/mm)')
 plt.title('Modulation Transfer Function')
 plt.show()
 
+"""STOP HERE FOR NOW"""
 sys.exit()
 
 
